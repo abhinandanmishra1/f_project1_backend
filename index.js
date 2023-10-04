240cc16ca006e4a21f38c64584c37b393b54bbf3
abhinandanmishra1 <abhinandanmishra360@gmail.com>
2023-10-04 12:24:38 +0530

    feat: openai integration

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const openai = require('openai');
-const cors = require('cors');
-require('dotenv').config();
+const Openai = require("openai");
+const cors = require("cors");
+require("dotenv").config();
 
-openai.apiKey = process.env.API_KEY;
+const openai = new Openai({
+  apiKey: process.env.API_KEY,
+  timeout: 5000,
+});
 
 app.use(
   cors({
     origin: [
       process.env.CLIENT_URL,
-      'http://localhost:5173',
-      'https://abhidocs.vercel.app',
-      'https://abhidocs.vercel.app/',
+      "http://localhost:5173",
+      "https://abhidocs.vercel.app",
+      "https://abhidocs.vercel.app/",
     ],
-    methods: 'GET,POST,PUT,DELETE',
+    methods: "GET,POST,PUT,DELETE",
   })
 );
 app.use(express.json());
 
 app.listen(3000, () => {
-  console.log('Server started on http://localhost:3000');
+  console.log("Server started on http://localhost:3000");
 });
 
-app.get('/', (req, res) => {
-  res.send('Api is working!');
+app.get("/", (req, res) => {
+  res.send("Api is working!");
 });
 
-app.post('/search', (req, res) => {
+app.post("/search", async (req, res) => {
   const { query } = req.body;
-  openai.completions
-    .create({
-      prompt: `search ${query}`,
-      model: 'text-davinci-002',
+  const content = query?.split('').reverse().join('') || ""
+  try {
+    const resy = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: query }],
+      model: "gpt-3.5-turbo",
       temperature: 0.5,
-    })
-    .then((response) => {
-      res.send(response.choices[0].text);
     });
+
+    const result = resy.choices[0].message.content;
+
+    return res.status(200).send({ result });
+  } catch (err) {
+    res.status(401).send({ error: err });
+  }
 });
